f9a8c6a1895a89424e1f5486b7e1874dedce7b7e
sanidhya0612 <102963033+sanidhya0612@users.noreply.github.com>
2024-02-10 21:38:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,11 +23,21 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+	video.size(700,600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
 }
 
+function modelLoaded()
+{
+  console.log("Model is Loaded!!!!!")
+}
 
 function draw(){
 
+  image(video,0,0,700,600);
  background(0); 
 
  fill("black");
